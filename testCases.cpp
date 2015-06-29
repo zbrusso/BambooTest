8219bd0581d20117fb5bbfc8d0416acfce21538b
zbrusso <zackbrusso@gmail.com>
2015-06-29 09:40:23 -0500

    second commit

--- testCases.cpp
+++ testCases.cpp
@@ -7,32 +7,53 @@
 #include <string>
 #include <iostream>
 #include <cstddef>
-#include <testing.cpp>
 using namespace std;
 
-main(){
+
+class evenIsTrue{
+  int number;
+  public:
+  evenIsTrue(){
+    number = 0;
+  }
+  evenIsTrue(int num){
+    number = num;
+  }
+   
+  bool isEven(){
+    if(number%2==0){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+};
+
+
+int main(){
 
   evenIsTrue test1(2);
   evenIsTrue test2(3);
   evenIsTrue test3(4);
   evenIsTrue test4(5);
 
-  if(test1.isEven==true){
+  if(test1.isEven()==true){
     cout<<"Working"<<endl;
   }
   else
     cout<<"Error"<<endl;
-  if(test2.isEven==false){
+  if(test2.isEven()==false){
     cout<<"Working"<<endl;
   }
-  else 
+  else
     cout<<"Error"<<endl;
-  if(test3.isEven==true){
+  if(test3.isEven()==true){
     cout<<"Working"<<endl;
     }
   else
     cout<<"Error"<<endl;
-  if(test4.isEven==false){
+  if(test4.isEven()==false){
     cout<<"Working"<<endl;
   }
   else
